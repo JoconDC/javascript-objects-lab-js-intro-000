f1e1ea951f622789706705637db5e89c4a7779d6
Jojo Concepcion <hsconcepcion@gmail.com>
2020-04-24 03:22:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,5 @@
 var recipe = new Object();
-  recipe.step1 = 'ingredients';
-  recipe.step2 = 'marinade';
-  recipe.step3 = 'grill';
 
-
-function updateRecipeWithStepAndMethod (recipe, step4, bake) {
+function updateRecipeWithStepAndMethod(recipe, step4, bake) {
 return Object.assign({}, recipe, {['step4']: 'bake'})
 }
